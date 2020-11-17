916d9e84ba3ddca2c7c02fe8fc88cb8fd73c392e
Dillon Hussey <dillonhussey@Dillons-MacBook-Pro.local>
2020-11-17 12:41:16 -0600

    Brewhaha is complete and commented

--- Javascript/brewhaha-DillonHussey/public/js/client.js
+++ Javascript/brewhaha-DillonHussey/public/js/client.js
@@ -1,18 +1,23 @@
 
+/*
+Dillon Hussey
+Brewhaha Project
+    Uses p5 library to do basic depth mapping according to brightness of pixels.
+    Maps pixels into 3d space to estimate shapes
+November 17 2020
+For Programming Languages with Brett Huffman
+*/
 
 let win;
 let fps = 60; //Frames per second
-let imgWidth = 0;
+let imgWidth = 0; 
 let imgHeigh = 0;
 let img;
-let boxes = [];
+let boxes = [];     //instantiates array for pixel being represented by a box in 3d
 
 class Boxmaker{
     constructor(r, g, b, x, y){
-        this.r = r;
-        this.g = g;
-        this.b = b;
-
+        //set color to rgb
         this.color = color(r,g,b)
 
         this.posX = x;
@@ -21,11 +26,7 @@ class Boxmaker{
         //this.posZ = hue(color(r,g,b))*2
         this.posZ = brightness(color(r,g,b))*2
     }
-    getR() {return this.r;}
-    getG() {return this.g;}
-    getB() {return this.b;}
-    
-
+    //getter methods
     getPosX() { return this.posX}
     getPosY() { return this.posY}
     getPosZ() { return this.posZ}
@@ -33,14 +34,13 @@ class Boxmaker{
     getColor() {return this.color}
 }
 
-
 function preload() {
-    img = loadImage('img/dog.jpg');
-    
-    
+    //load image
+    img = loadImage('img/shoes.jpg');
 }
 
 function setup() {
+    //set up window and canvas and pixelDensity to 1
     rectMode(CENTER);
     win = {width: 1000, height: 1000};
     pixelDensity(1);
@@ -48,17 +48,19 @@ function setup() {
     canvas.parent('sketch-holder');
 
 
-
+    //sets framerate to 60 fps
     frameRate(fps);
 
-
+    //loads pixels of image into img.pixels array
     img.loadPixels();
 
 
     //Set scale depending on dimensions of image. (small images work best ~ 100x100  needs scale 1)
     let scale = 1
    
+    //run through pixel array putting pixels into boxes array. (increase by 4 as each pixel has r,b,g,a)
     for(let i = 0; i < img.pixels.length; i+=4*scale) {
+        //push new boxmaker object into boxes using rbg and position in array
         boxes.push(new Boxmaker(img.pixels[i], img.pixels[i+1], img.pixels[i+2], (i % (img.width*4))/scale, Math.floor(i/(img.width))/scale ))
     }
 
@@ -68,22 +70,26 @@ function setup() {
 }
 
 function draw(){
+    //set gray background
     background(100);
-
-    //console.log(boxes[150].getColor())
     
+    //run through boxes drawing each box which reps a pixel
     for(let p = 0; p<boxes.length; p++){
         push();
+        //add reference to decrease runtime
         let b = boxes[p];
+        //set position to x,y,z of box object
         translate(b.getPosX()-400, b.getPosY()-400, b.getPosZ());
-        //stroke(b.getColor());
+        //noStroke to decrease runtime
         noStroke();
+        //set color
         fill(b.getColor());
+        //draw box of size 4
         box(4);
 
         pop();
     }
         
-
+    //allows 3d orbit control
     orbitControl();
 }
